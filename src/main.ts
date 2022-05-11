a2c4834653c6cb85c72e9802b9dde156662e55cf
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2022-05-11 09:47:47 +0300

    Fix

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
 /// <reference path="./types/index.d.ts" />
-/// <reference types="@rana-mc/fabric/build/main" />
-/// <reference types="@rana-mc/forge/build/main" />
 import { startApiServer } from './server';
 
 const main = async () => {
